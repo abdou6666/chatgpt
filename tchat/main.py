7c85a66861320d7ca42984f18b1032f62ef88a9b
abdou6666 <abdousfayhi12@gmail.com>
2024-01-12 21:45:33 +0100

    chat bot

---
+++ tchat/main.py
@@ -0,0 +1,40 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import HumanMessagePromptTemplate, MessagesPlaceholder, ChatPromptTemplate
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory,ConversationSummaryMemory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI(
+    verbose=True
+)
+
+# memory = ConversationBufferMemory(
+    # memory_key="messages",
+    # return_messages=True,
+    # chat_memory=FileChatMessageHistory("messages.json")
+    # )
+memory = ConversationSummaryMemory(
+    memory_key="messages",
+    return_messages=True,
+    llm=chat
+    )
+
+prompt = ChatPromptTemplate(
+    input_variable=["content","messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory,
+    verbose=True
+)
+while True:
+    content = input(">> ")
+    result = chain({"content":content})
+    print(result["text"])
